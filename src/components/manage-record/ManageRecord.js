8e7cdf9bc2b45432e237621621b361d5b35f8f74
lcsaria <lmarlsaria21@gmail.com>
2021-12-27 08:18:13 +0800

    update

--- src/components/manage-record/ManageRecord.js
+++ src/components/manage-record/ManageRecord.js
@@ -16,54 +16,77 @@ function ManageRecord() {
             <div className="container">
               
             <div className="card mb-1 d-block">
-              <div className="card-body">
-                  <span className="card-title card text-center p-2"
+            <div className="card-body">
+            <span className="card-title card text-center p-2"
                   style={{position: "relative", top: "-30px",  border: 
                   "1px black solid",display: 'block',
                   width: '200px'}}>
-                    <b className="text-uppercase text-dark">Manage Record</b>
+                    <b className="text-uppercase text-dark">Manage User</b>
                   </span>
+        <div className="row">
+          <div className="col-md-6 text-nowrap">
+            <div
+              id="dataTable_length"
+              className="dataTables_length"
+              aria-controls="dataTable"
+            />
+          </div>
+          <div className="col-md-6">
+            <div
+              className="text-md-right dataTables_filter"
+              id="dataTable_filter"
+            >
+              <label>
+                <input
+                  type="search"
+                  className="form-control form-control-sm"
+                  aria-controls="dataTable"
+                  placeholder="Search..."
+                />
+                <span className="search-icon">
+                  <i class="fas fa-search"></i>
+                </span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div
+          className="table-responsive table mt-2"
+          id="dataTable-1"
+          role="grid"
+          aria-describedby="dataTable_info"
+        >
 
-                  <table class="table">
-                    <thead>
-                      <tr>
-                        <th scope="col">File Number</th>
+              <table className="table my-0" id="dataTable">
+              <thead>
+              <tr>
+              <th scope="col">File Number</th>
                         <th scope="col">Name</th>
-                        <th scope="col">Type</th>
                         <th scope="col">Author</th>
+                        <th scope="col">Type</th>
                         <th scope="col">Created by</th>
                         <th scope="col">Updated by</th>
                         <th scope="col">Action</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="row">1</th>
+              </tr>
+            </thead>
+            <tbody>
+            <th scope="row">1</th>
                         <td>Memo no. 1</td>
-                        <td>Memo</td>
                         <td>LBPascua</td>
+                        <td>Memo</td>
                         <td>01/01/21 10:00AM</td>
                         <td>01/01/21 10:00AM</td>
                         <td>
                           <a href="/manage-record" ><i className="fas fa-users mr-4"/></a>
                           <i className="fas fa-trash mr-4"/>
                         </td>
-                      </tr>
-                      <tr>
-                      <th scope="row">1</th>
-                        <td>Memo no. 1</td>
-                        <td>Memo</td>
-                        <td>LBPascua</td>
-                        <td>01/01/21 10:00AM</td>
-                        <td>01/01/21 10:00AM</td>
-                        <td>
-                          <a href="/manage-record" ><i className="fas fa-users mr-4"/></a>
-                          <i className="fas fa-trash mr-4"/>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-              </div>
+            </tbody>
+            <tfoot>
+              <tr />
+            </tfoot>
+            </table>
+        </div>
+      </div>
              </div>
             </div>
           </div>
--- src/components/templates/Navbar.js
+++ src/components/templates/Navbar.js
@@ -1,43 +1,55 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { useHistory } from 'react-router-dom';
+import {Nav, Navbar, Container, NavDropdown} from 'react-bootstrap';
+import logo from '../../assets/logo.png';
+import logomini from '../../assets/logomini.png';
 
-function Navbar() {
+const Navbars = () => {
+
   const history = useHistory();
 
-  const logOut = () => {
+  const logOut = async () => {
     alert("Thank you!");
-    localStorage.clear();
-    history.push("/login");
+      localStorage.clear();
+      history.push("/login");
   }
-  
-  console.log(window.location.pathname)
+
     return (
-    <nav
-        className="navbar navbar-light navbar-expand bg-white shadow topbar static-top"
-        style={{ boxShadow: "0px 4px 5px"}}
-      >
-        <div className="container-fluid" >
-          <ul className="navbar-nav ml-auto">
-            <div className="d-none d-sm-block " />
-            <li className="nav-item  no-arrow">
-              <div className="nav-item no-arrow">
-                <a
-                  className="btn-outline-light"
-                  aria-expanded="false"
-                  onClick={logOut}
-                >
-                  <span className="d-none d-lg-inline mb-4 mr-2 text-gray-600 small">
-                  <span className="fa fa-sign-out mr-2" aria-hidden="true"/>
-                    Logout
-                  </span>
-                </a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </nav>
+
+      <Navbar bg="white" expand="lg" sticky="top"> 
+        <Container fluid>
+        <Navbar.Brand href="/dashboard" className="meowlogo">
+          <img
+            src={logomini}
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+        />
+        </Navbar.Brand>
+        <Navbar.Brand href="/dashboard" className="logomeow d-lg-none">
+          <img
+            src={logo}
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+        />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <div className = "ms-auto"/>
+            <Nav>   
+              <Nav.Link className="d-lg-none" href="/dashboard"><i className="fas fa-home mr-3"/>Home</Nav.Link>
+              <Nav.Link className="d-lg-none" href="/upload-record"><i className="fas fa-paperclip mr-3"/>Upload Record</Nav.Link>
+              <Nav.Link className="d-lg-none" href="/manage-record"><i className="fas fa-table mr-3"/>Manage Record</Nav.Link>
+              <NavDropdown title={<div style={{display: "inline-block"}}><i class="fas fa-user mr-2"/> {'User'}</div> } id="basic-nav-dropdown" class>
+                <NavDropdown.Item href="/settings">Account Settings</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item onClick={logOut}>Logout</NavDropdown.Item>
+              </NavDropdown>
+              
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     )
 }
-
-export default Navbar
+export default Navbars
